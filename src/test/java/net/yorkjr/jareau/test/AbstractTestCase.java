7b0c48bbe9cd9115e6d65e2c237239470aceffeb
Rick Wang <rickwangtw@hotmail.com>
2015-01-18 04:10:41 +0800

    AbstractTestCase: create a Logger for every test case

--- src/test/java/net/yorkjr/jareau/test/AbstractTestCase.java
+++ src/test/java/net/yorkjr/jareau/test/AbstractTestCase.java
@@ -1,5 +1,6 @@
 package net.yorkjr.jareau.test;
 
+import org.apache.log4j.Logger;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -7,4 +8,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:web/WEB-INF/applicationContext.xml"})
 public class AbstractTestCase {
+    protected final Logger log = Logger.getLogger(getClass());
 }
